999dc2a1fc03b9a2d693c3121f6d2ee027eaae76
Custy Home <custardcs@gmail.com>
2024-02-02 16:26:05 +0000

    aasd

--- lib/main.dart
+++ lib/main.dart
@@ -1,133 +1,417 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
-import 'package:flutter/material.dart';
-// import 'package:image_picker/image_picker.dart';
-import 'package:http/http.dart' as http;
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:photo_manager/photo_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:permission_handler/permission_handler.dart';
+import 'package:photo_gallery/photo_gallery.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: UploadScreen(),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class UploadScreen extends StatefulWidget {
-  @override
-  _UploadScreenState createState() => _UploadScreenState();
-}
-
-class _UploadScreenState extends State<UploadScreen> {
-  String _response = '';
-  List<String> uploadedHashes = [];
-  int totalPhotos = 0;
+class _MyAppState extends State<MyApp> {
+  List<Album>? _albums;
+  bool _loading = false;
+  StreamSubscription<ConnectivityResult>? _connectivitySubscription;
+  String _response = 'TEST TEST';
+  List uploadedHashes = [];
   int photosUploaded = 0;
+  int totalPhotos = 0;
 
-  Future<bool> _requestPermission() async {
-    var result = await PhotoManager.requestPermissionExtend();
-    return result.isAuth;
+  @override
+  void initState() {
+    super.initState();
+    _loading = true;
+    initAsync();
+
+    // Add a listener to check for network connectivity changes
+    _connectivitySubscription =
+        Connectivity().onConnectivityChanged.listen((result) {
+      if (result == ConnectivityResult.wifi ||
+          result == ConnectivityResult.mobile) {
+        // Start uploading photos when there is a network connection
+        _uploadAllPhotos();
+      }
+    });
   }
 
-  Future<List<AssetEntity>> _fetchAllPhotos() async {
-    if (!(await _requestPermission())) {
-      return [];
-    }
-
-    // Obtain the album list
-    List<AssetPathEntity> albums =
-        await PhotoManager.getAssetPathList(onlyAll: true);
-    List<AssetEntity> allPhotos = [];
-
-    // Define the batch size
-    int batchSize =
-        500; // Adjust this number based on performance and memory usage
-    int currentPage = 0;
-    bool hasMore = true;
-
-    while (hasMore) {
-      List<AssetEntity> photos =
-          await albums[0].getAssetListPaged(page: currentPage, size: batchSize);
-      allPhotos.addAll(photos);
-
-      // Check if there are more photos to fetch
-      if (photos.length < batchSize) {
-        hasMore = false;
+  Future<void> initAsync() async {
+    if (await _promptPermissionSetting()) {
+      bool isConnected = await _checkConnectivity();
+      if (isConnected) {
+        List<Album> albums = await PhotoGallery.listAlbums();
+        setState(() {
+          _albums = albums;
+          _loading = false;
+        });
       } else {
-        currentPage++;
+        print("No internet connection available");
       }
     }
-
-    return allPhotos;
-  }
-
-  Future<void> _uploadAllPhotos() async {
-  if (await isConnected()) {
-    List<AssetEntity> photos = await _fetchAllPhotos();
-    totalPhotos = photos.length;  // Set the total number of photos
-
-    for (var photo in photos) {
-      var file = await photo.file;
-      if (file != null) {
-        var request = http.MultipartRequest(
-            'POST', Uri.parse('http://192.168.98.101:3000/upload'));
-        request.files.add(await http.MultipartFile.fromPath('file', file.path));
-
-        var res = await request.send();
-        final resData = await res.stream.bytesToString();
-        var data = json.decode(resData);
-        if (data['fileHash'] != null) {
-          uploadedHashes.add(data['fileHash']);
-          photosUploaded++;  // Increment the count of uploaded photos
-        }
-      }
-      setState(() {
-        _response = '$photosUploaded of $totalPhotos uploaded';
-      });
-    }
     setState(() {
-      _response = 'All photos uploaded. Hashes: $uploadedHashes';
-    });
-  } else {
-    setState(() {
-      _response = 'No internet connection available';
+      _loading = false;
     });
   }
-}
 
-
-  Future<bool> isConnected() async {
+  Future<bool> _checkConnectivity() async {
     var connectivityResult = await Connectivity().checkConnectivity();
-    if (connectivityResult == ConnectivityResult.mobile ||
-        connectivityResult == ConnectivityResult.wifi) {
-      return true;
+    return connectivityResult == ConnectivityResult.mobile ||
+        connectivityResult == ConnectivityResult.wifi;
+  }
+
+  Future<bool> _promptPermissionSetting() async {
+    if (Platform.isIOS) {
+      if (await Permission.photos.request().isGranted ||
+          await Permission.storage.request().isGranted) {
+        return true;
+      }
+    }
+    if (Platform.isAndroid) {
+      if (await Permission.storage.request().isGranted ||
+          await Permission.photos.request().isGranted &&
+              await Permission.videos.request().isGranted) {
+        return true;
+      }
     }
     return false;
   }
 
+  Future<void> _uploadAllPhotos() async {
+    if (await _checkConnectivity()) {
+      try {
+        if (_albums != null) {
+          for (var album in _albums!) {
+            // Inside this loop, you can access the photos in the current album.
+            // You can retrieve the photos using the `album.listMedia()` method.
+            // This method returns a `MediaPage` object containing the photos in the album.
+            print(album);
+            // For example, you can fetch the photos for the current album like this:
+            MediaPage mediaPage = await album.listMedia();
+            List<Medium>? photos = mediaPage.items;
+
+            // Now, you have access to the `photos` variable, which contains the photos
+            // in the current album. You can loop through these photos and upload them.
+
+            for (var photo in photos) {
+              // Upload the photo to the server here.
+              // You can use the `_uploadPhoto` method or any other method you prefer.
+              var file = await PhotoGallery.getFile(mediumId: photo.id);
+
+              var request = http.MultipartRequest(
+                'POST',
+                Uri.parse('http://192.168.98.101:3000/upload'),
+              );
+              request.files.add(await http.MultipartFile.fromPath('file', file.path));
+
+              var res = await request.send();
+              final resData = await res.stream.bytesToString();
+              var data = json.decode(resData);
+              if (data['fileHash'] != null) {
+                uploadedHashes.add(data['fileHash']);
+                photosUploaded++; // Increment the count of uploaded photos
+              }
+              setState(() {
+                _response = '$photosUploaded of $totalPhotos uploaded';
+              });
+            }
+          }
+        }
+      } catch (e) {
+        print('Error uploading photos: $e');
+      }
+      // setState(() {
+      //   _response = 'All photos uploaded. Hashes: $uploadedHashes';
+      // });
+    } else {
+      setState(() {
+        _response = 'No internet connection available';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Upload File')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Photo Backup'),
+        ),
+        body: _loading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : LayoutBuilder(
+                builder: (context, constraints) {
+                  double gridWidth = (constraints.maxWidth - 20) / 3;
+                  double gridHeight = gridWidth + 33;
+                  double ratio = gridWidth / gridHeight;
+                  return Container(
+                    padding: EdgeInsets.all(5),
+                    child: GridView.count(
+                      childAspectRatio: ratio,
+                      crossAxisCount: 3,
+                      mainAxisSpacing: 5.0,
+                      crossAxisSpacing: 5.0,
+                      children: <Widget>[
+                        ...?_albums?.map(
+                          (album) => GestureDetector(
+                            onTap: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (context) => AlbumPage(album)),
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(5.0),
+                                  child: Container(
+                                    color: Color.fromARGB(255, 17, 184, 67),
+                                    height: gridWidth,
+                                    width: gridWidth,
+                                    child: FadeInImage(
+                                      fit: BoxFit.cover,
+                                      placeholder:
+                                          MemoryImage(kTransparentImage),
+                                      image: AlbumThumbnailProvider(
+                                        album: album,
+                                        highQuality: true,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  alignment: Alignment.topLeft,
+                                  padding: EdgeInsets.only(left: 2.0),
+                                  child: Text(
+                                    album.name ?? "Unnamed Album",
+                                    maxLines: 1,
+                                    textAlign: TextAlign.start,
+                                    style: TextStyle(
+                                      height: 1.2,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  alignment: Alignment.topLeft,
+                                  padding: EdgeInsets.only(left: 2.0),
+                                  child: Text(
+                                    album.count.toString(),
+                                    textAlign: TextAlign.start,
+                                    style: TextStyle(
+                                      height: 1.2,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  child: Text(_response),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+      ),
+    );
+  }
+}
+
+class AlbumPage extends StatefulWidget {
+  final Album album;
+
+  AlbumPage(Album album) : album = album;
+
+  @override
+  State<StatefulWidget> createState() => AlbumPageState();
+}
+
+class AlbumPageState extends State<AlbumPage> {
+  List<Medium>? _media;
+
+  @override
+  void initState() {
+    super.initState();
+    initAsync();
+  }
+
+  void initAsync() async {
+    MediaPage mediaPage = await widget.album.listMedia();
+    setState(() {
+      _media = mediaPage.items;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: Text(widget.album.name ?? "Unnamed Album"),
+        ),
+        body: GridView.count(
+          crossAxisCount: 3,
+          mainAxisSpacing: 1.0,
+          crossAxisSpacing: 1.0,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: _uploadAllPhotos,
-              child: Text('Upload All Images'),
+            ...?_media?.map(
+              (medium) => GestureDetector(
+                onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => ViewerPage(medium)),
+                ),
+                child: Stack(
+                  children: [
+                    Container(
+                      color: Color.fromARGB(255, 8, 210, 236),
+                      child: FadeInImage(
+                        fit: BoxFit.cover,
+                        placeholder: MemoryImage(kTransparentImage),
+                        image: ThumbnailProvider(
+                          mediumId: medium.id,
+                          mediumType: medium.mediumType,
+                          highQuality: true,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 5,
+                      right: 5,
+                      child: Container(
+                        width: 10,
+                        height: 10,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            SizedBox(height: 20),
-            Text(_response),
           ],
         ),
       ),
     );
+  }
+}
+
+class ViewerPage extends StatelessWidget {
+  final Medium medium;
+
+  ViewerPage(Medium medium) : medium = medium;
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime? date = medium.creationDate ?? medium.modifiedDate;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () => Navigator.of(context).pop(),
+            icon: Icon(Icons.arrow_back_ios),
+          ),
+          title: date != null ? Text(date.toLocal().toString()) : null,
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: medium.mediumType == MediumType.image
+              ? GestureDetector(
+                  onTap: () async {
+                    PhotoGallery.deleteMedium(mediumId: medium.id);
+                  },
+                  child: FadeInImage(
+                    fit: BoxFit.cover,
+                    placeholder: MemoryImage(kTransparentImage),
+                    image: PhotoProvider(mediumId: medium.id),
+                  ),
+                )
+              : VideoProvider(
+                  mediumId: medium.id,
+                ),
+        ),
+      ),
+    );
+  }
+}
+
+class VideoProvider extends StatefulWidget {
+  final String mediumId;
+
+  const VideoProvider({
+    required this.mediumId,
+  });
+
+  @override
+  _VideoProviderState createState() => _VideoProviderState();
+}
+
+class _VideoProviderState extends State<VideoProvider> {
+  VideoPlayerController? _controller;
+  File? _file;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      initAsync();
+    });
+    super.initState();
+  }
+
+  Future<void> initAsync() async {
+    try {
+      _file = await PhotoGallery.getFile(mediumId: widget.mediumId);
+      _controller = VideoPlayerController.file(_file!);
+      _controller?.initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+    } catch (e) {
+      print("Failed : $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _controller == null || !_controller!.value.isInitialized
+        ? Container()
+        : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: _controller!.value.aspectRatio,
+                child: VideoPlayer(_controller!),
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    _controller!.value.isPlaying
+                        ? _controller!.pause()
+                        : _controller!.play();
+                  });
+                },
+                child: Icon(
+                  _controller!.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                ),
+              ),
+            ],
+          );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus
-import file_selector_macos
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
